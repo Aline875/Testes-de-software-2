7340c6403f2394895b43cc84ce5eb41abf40d76f
Aline <113183673+Aline875@users.noreply.github.com>
2024-05-15 14:16:36 -0300

    .

--- projetoDeTestes/obj/Debug/net8.0/projetoDeTestes.AssemblyInfo.cs
+++ projetoDeTestes/obj/Debug/net8.0/projetoDeTestes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetoDeTestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a7e37aee29d93dffe66d01ae1f6fa8753b3184")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetoDeTestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetoDeTestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
